9908bf3df35fec88fee28c800705a80beed48df9
MatteDev02 <62772891+MattDEV02@users.noreply.github.com>
2021-02-28 12:30:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ const s = (selector, i = 0) => document.querySelectorAll(selector)[i] ;
 
 const trigger_click = elements => elements.map(el => el.click()); 
 
-const classes = ['.wpO6b  ','.coreSpriteRightPaginationArrow'];
+const classes = ['.wpO6b  ', '.coreSpriteRightPaginationArrow'];
 
 const insta_bot = n => {
-	let 
-		cuore = null , 
-		freccia = null;
+   let 
+      cuore = null , 
+      freccia = null;
    window.setInterval(() => { 
-	   cuore = s(classes[0],1) ; 
-	   freccia = s(classes[1]); 
-	   trigger_click([cuore,freccia]);
+      cuore = s(classes[0],1) ; 
+      freccia = s(classes[1]); 
+      trigger_click([cuore,freccia]);
    }, n);
 };
 
 
-insta_bot(750); //
+insta_bot(750); // You can change this.
