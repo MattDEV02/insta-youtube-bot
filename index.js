ff3d205a6c79b43052f908b296e02b50b0634f9f
MattDEV02 <matteolambertucci3@gmail.com>
2021-02-28 12:27:59 +0100

    push

---
+++ index.js
@@ -0,0 +1,19 @@
+const s = (selector, i = 0) => document.querySelectorAll(selector)[i] ;
+
+const trigger_click = elements => elements.map(el => el.click()); 
+
+const classes = ['.wpO6b  ','.coreSpriteRightPaginationArrow'];
+
+const insta_bot = n => {
+	let 
+		cuore = null , 
+		freccia = null;
+   window.setInterval(() => { 
+	   cuore = s(classes[0],1) ; 
+	   freccia = s(classes[1]); 
+	   trigger_click([cuore,freccia]);
+   }, n);
+};
+
+
+insta_bot(750); //
