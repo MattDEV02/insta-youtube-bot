ddac4adc4c2e206eab00ad4c579d132e52da547d
MatteDev02 <62772891+MattDEV02@users.noreply.github.com>
2021-03-05 12:58:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const s = (selector, i = 0) => document.querySelectorAll(selector)[i] ;
 
 const trigger_click = elements => elements.map(el => el.click()); 
 
-const classes = ['.wpO6b  ', '.coreSpriteRightPaginationArrow'];
+const classes = ['.wpO6b  ', '.coreSpriteRightPaginationArrow', '.yt-simple-endpoint'];
 
 const insta_bot = n => {
    let 
@@ -11,13 +11,13 @@ const insta_bot = n => {
    window.setInterval(() => { 
       like = s(classes[0],1) ; 
       arrow = s(classes[1]); 
-      trigger_click([like,arrow]);
+      trigger_click([like, arrow]);
    }, n);
 };
 
 const youtube_bot = n => {
-   let dislike = s('.yt-simple-endpoint',10);
-   window.setInterval(() => dislike.click(),n);
+   let dislike = s(classes[2],10);
+   window.setInterval(() => trigger_click([dislike]),n);
 };
 
 //insta_bot(2000); 
